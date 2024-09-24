086d2b7aa6803500027b93c93d964678ed1346e6
Ismail424 <cicas03@hotmail.com>
2024-09-24 15:18:26 +0200

    Added tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>FlameWatch</title>
+</svelte:head>
+<slot />
--- src/routes/test/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    let x = 1;  
-</script>
-
-<p>hej {x}</p>
-
-<button on:click={() => x += 1}>
-    Increment
-</button>
