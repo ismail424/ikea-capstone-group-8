79bfe4942a95f9f947d02a43653262247baf71fc
TobiasOPettersson <94981967+TobiasOPettersson@users.noreply.github.com>
2024-09-29 23:34:14 +0200

    config update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getDatabase, ref, set, onValue } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMjywc2U5s3t3ie12Cw6uh9SUcHdM7_sQ",
-  authDomain: "firewatch-server.firebaseapp.com",
-  databaseURL: "https://firewatch-server-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "firewatch-server",
-  storageBucket: "firewatch-server.appspot.com",
-  messagingSenderId: "75589631691",
-  appId: "1:75589631691:web:e28f6a106110b0cbde654e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
