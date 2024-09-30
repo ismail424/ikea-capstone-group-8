084f7791c6a16ac694be68b0d69a07127369762a
Ismail Sacic <cicas03@hotmail.com>
2024-09-30 11:25:09 +0200

    Fixed Simulation

--- src/routes/simulation/+page.server.ts
+++ src/routes/simulation/+page.server.ts
@@ -1,11 +1,5 @@
 import type { PageServerLoad } from './$types';
 
-let x : number = 0;
 export const load = (async () => {
-
-    x += 1;
-    console.log(x);
-
-
     return {};
 }) satisfies PageServerLoad;
