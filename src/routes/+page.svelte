48f89cb68c3cca6001388e7791b2bb5e9e85d8c2
Ken Phu <kenz.phu@gmail.com>
2024-11-01 14:36:34 +0100

    Better compressed mp4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 				<h3 class="text-center text-2xl font-bold text-gray-800">Project Demo</h3>
 			</div>
 			<video width="100%" height="100%" controls>
-				<source src="Final_Pitch_Video_Group_8_FlameWatch.mp4" type="video/mp4" />
+				<source src="final_pitch.mp4" type="video/mp4" />
 				Your browser does not support the video tag.
 			</video>
 		</div>
